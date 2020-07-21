289807b0c0b798fc4203f7a77b259ee697a48203
Dave Dave <ross-hutchison-uni@virginmedia.com>
2020-07-21 18:30:51 +0100

    made item abstract and added the methods for using items alone and with obstacles

--- src/item.java
+++ src/item.java
@@ -1,14 +1,16 @@
-public class item {
+public abstract class item {
     private String itemIs;
     private String description;
     private String feelsLike;
     private String tastesLike;
+    private String usedAlone;
 
-    public item(String itemIs, String description, String feelsLike, String tastesLike) {
+    public item(String itemIs, String description, String feelsLike, String tastesLike, String usedAlone) {
         this.itemIs = itemIs;
         this.description = description;
         this.feelsLike = feelsLike;
         this.tastesLike = tastesLike;
+        this.usedAlone = usedAlone;
     }
 
     public String getItemIs() {
@@ -25,5 +27,13 @@ public class item {
 
     public void lick() {
         System.out.println(this.tastesLike);
+    }
+
+    public void use(){
+        System.out.println(this.usedAlone);
+    }
+
+    public void useWith(obstacle useOn){
+        useOn.resolve(this);
     }
 }
--- src/obstacle.java
+++ src/obstacle.java
@@ -1,10 +1,14 @@
-public class obstacle extends item {
+public abstract class obstacle extends item {
     private boolean solved;
     private item solvedBy;
 
-    public obstacle(item solvedBy, String itemIs, String description, String feelsLike, String tastesLike) {
-        super(itemIs, description, feelsLike, tastesLike);
+    public obstacle(item solvedBy, String itemIs, String description, String feelsLike, String tastesLike, String usedAlone) {
+        super(itemIs, description, feelsLike, tastesLike, usedAlone);
         this.solved = false;
         this.solvedBy = solvedBy;
+    }
+
+    public void resolve(item usedWith){
+
     }
 }
