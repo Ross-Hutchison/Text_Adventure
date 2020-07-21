62710d3bf3cd13f556835416befdaed9019064e3
Dave Dave <ross-hutchison-uni@virginmedia.com>
2020-07-21 18:46:28 +0100

    forgot to make obstacle.resolve actually do anything - now it does

--- src/obstacle.java
+++ src/obstacle.java
@@ -1,14 +1,24 @@
 public abstract class obstacle extends item {
     private boolean solved;
     private item solvedBy;
+    private String resolvedMsg;
+    private String resolveFailMsg;
 
-    public obstacle(item solvedBy, String itemIs, String description, String feelsLike, String tastesLike, String usedAlone) {
+    public obstacle(item solvedBy, String resolvedMsg, String resolveFailMsg, String itemIs, String description, String feelsLike, String tastesLike, String usedAlone) {
         super(itemIs, description, feelsLike, tastesLike, usedAlone);
         this.solved = false;
         this.solvedBy = solvedBy;
+        this.resolvedMsg = resolvedMsg;
+        this.resolveFailMsg = resolveFailMsg;
     }
 
     public void resolve(item usedWith){
-
+        if(!solved && usedWith == solvedBy) {
+            this.solved = true;
+            System.out.println(this.resolvedMsg);
+        }
+        else if(!solved) {
+            System.out.println(resolvedMsg);
+        }
     }
 }
